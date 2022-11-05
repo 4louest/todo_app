e247ba2964b05d08d9c04f362ca337ab6d29123d
4louest <alex.saury@gmail.com>
2022-11-05 20:41:36 +0100

    finalize crud

--- backend/src/model/todo.rs
+++ backend/src/model/todo.rs
@@ -1,5 +1,5 @@
 use super::db::Db;
-use crate::model;
+use crate::{model, security::UserCtx};
 use sqlb::{HasFields, Raw};
 
 // region:  Todo Types
@@ -14,7 +14,6 @@ pub struct Todo {
 #[derive(sqlb::Fields, Default, Debug, Clone)]
 pub struct TodoPatch {
     title: Option<String>,
-    cid: Option<i64>,
     status: Option<TodoStatus>,
 }
 
@@ -32,28 +31,91 @@ sqlb::bindable!(TodoStatus);
 pub struct TodoMac;
 
 impl TodoMac {
-    pub async fn create(db: &Db, data: TodoPatch) -> Result<Todo, model::Error> {
+    const TABLE: &'static str = "todo";
+    const COLUMNS: &'static [&'static str] = &["id", "cid", "title", "status"];
+}
+
+impl TodoMac {
+    pub async fn create(db: &Db, utx: &UserCtx, data: TodoPatch) -> Result<Todo, model::Error> {
+        let mut fields = data.fields();
+        fields.push(("cid", 123).into());
+
         let sb = sqlb::insert()
-            .table("todo")
-            .data(data.fields())
-            .returning(&["id", "cid", "title", "status"]);
+            .table(Self::TABLE)
+            .data(fields)
+            .returning(Self::COLUMNS);
 
         let todo = sb.fetch_one(db).await?;
 
         Ok(todo)
     }
 
-    pub async fn list(db: &Db) -> Result<Vec<Todo>, model::Error> {
-        let sql = "SELECT id, cid, title, status FROM todo ORDER BY id DESC";
+    pub async fn get(db: &Db, _utx: &UserCtx, id: i64) -> Result<Todo, model::Error> {
+        let sb = sqlb::select()
+            .table(Self::TABLE)
+            .columns(Self::COLUMNS)
+            .and_where_eq("id", id);
 
-        // build sqlx query
-        let query = sqlx::query_as(&sql);
+        let result = sb.fetch_one(db).await;
+        handle_fetch_one_result(result, Self::TABLE, id)
+    }
+
+    pub async fn update(
+        db: &Db,
+        utx: &UserCtx,
+        id: i64,
+        data: TodoPatch,
+    ) -> Result<Todo, model::Error> {
+        let mut fields = data.fields();
+        // augment the fields with the cid/ctime
+        fields.push(("mid", utx.user_id).into());
+        fields.push(("ctime", Raw("now()")).into());
+
+        let sb = sqlb::update()
+            .table(Self::TABLE)
+            .data(fields)
+            .and_where_eq("id", id)
+            .returning(Self::COLUMNS);
+
+        let result = sb.fetch_one(db).await;
+        handle_fetch_one_result(result, Self::TABLE, id)
+    }
+
+    pub async fn list(db: &Db, utx: &UserCtx) -> Result<Vec<Todo>, model::Error> {
+        let sb = sqlb::select()
+            .table(Self::TABLE)
+            .columns(Self::COLUMNS)
+            .order_by("!id");
         // execute query
-        let todos = query.fetch_all(db).await?;
+        let todos = sb.fetch_all(db).await?;
         Ok(todos)
+    }
+
+    pub async fn delete(db: &Db, _utx: &UserCtx, id: i64) -> Result<Todo, model::Error> {
+        let sb = sqlb::delete()
+            .table(Self::TABLE)
+            .returning(Self::COLUMNS)
+            .and_where_eq("id", id);
+
+        let result = sb.fetch_one(db).await;
+
+        handle_fetch_one_result(result, Self::TABLE, id)
     }
 }
 // endregion:  TodoMac
+
+// region:    Utils
+fn handle_fetch_one_result(
+    result: Result<Todo, sqlx::Error>,
+    typ: &'static str,
+    id: i64,
+) -> Result<Todo, model::Error> {
+    result.map_err(|sqlx_error| match sqlx_error {
+        sqlx::Error::RowNotFound => model::Error::EntityNotFound(typ, id.to_string()),
+        other => model::Error::SqlxError(other),
+    })
+}
+// endregion: Utils
 
 #[cfg(test)]
 #[path = "../_tests/model_todo.rs"]
